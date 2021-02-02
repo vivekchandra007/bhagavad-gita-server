180d6386f6c0239143ca03a104d2a80c251aeb3d
Vivek Chandra <vivek.chandra@t-systems.com>
2021-02-03 01:40:18 +0530

    vercel testing

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const dbSchema = require("./schema/db-schema");
 
 const app = express();
-
-app.get("/", (req, res) => res.send("Home Page Route"));
-
-app.get("/about", (req, res) => res.send("About Page Route"));
-
-app.get("/portfolio", (req, res) => res.send("Portfolio Page Route"));
-
-app.get("/contact", (req, res) => res.send("Contact Page Route"));
-
 const port = process.env.PORT || 3000;
+
+// No access without a valid access token (one received after calling calling /oauth2/v1/ route)
+//app.use(require("./middlewares/auth"));
+
+app.get("/auth/oauth/v1/token/", (req, res) => {
+  res.status(200).json({
+    token:
+      "HareKrishnaHareKrishnaKrishnaKrishnaHareHareHareRamHareRamRamRamHareHare",
+  });
+});
+
+app.use(
+  "/graphql/v1",
+  graphqlHTTP({
+    schema: dbSchema,
+    graphiql: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hare Krishna !!!" });
+});
 
 app.listen(port, () =>
   console.log(`Server running on ${port}, http://localhost:${port}`)
