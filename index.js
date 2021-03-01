d957e1bddeeebabb311252052b6d0c270ba1427f
Vivek Chandra <vivek.chandra@t-systems.com>
2021-03-02 01:06:24 +0530

    test includeFile vercel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const sass = require("sass");
 const fs = require("fs");
+const { join } = require("path");
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const ObjectId = require("mongodb").ObjectID;
@@ -12,26 +13,12 @@ const tokenFactory = require("./security/token-factory");
 
 const bhagavadGitaDB = require("./db/bhagavad-gita-db");
 
-
-const glob = require("glob");
-
-var getDirectories = function (src, callback) {
-  glob(src + "/**/*", callback);
-};
-getDirectories(".", function (err, res) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log(res);
-  }
-});
-
 // first compile sass file to a css one
-// const scssData = fs.readFileSync("./scss/index.scss", "utf8");
-// const compiledCSS = sass.renderSync({
-//   data: scssData,
-// }); 
-// fs.writeFileSync("./public/styles/index.css", compiledCSS.css);
+const scssData = fs.readFileSync(join(__dirname, "scss", "index.scss"), "utf8");
+const compiledCSS = sass.renderSync({
+  data: scssData,
+});
+fs.writeFileSync("./public/styles/index.css", compiledCSS.css);
 
 const app = express();
 const port = process.env.PORT || constants.LOCALHOST_PORT;
@@ -105,13 +92,6 @@ app.get("/api/rest/v1", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  getDirectories(".", function (err, res) {
-    if (err) {
-      console.log("Error", err);
-    } else {
-      console.log(res);
-    }
-  });
   res.status(200).json({ message: "Hare Krishna !!!" });
 });
 
