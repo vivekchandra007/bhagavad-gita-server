951945a3145b7e4db3a24f931f12d86ffe9bd72a
Vivek Chandra <vivek.chandra@t-systems.com>
2021-05-02 02:35:04 +0530

    Form landing page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,5 +4,24 @@ const app = new Vue({
   data: {
     name: "Bhagavad Gita Website & APIs",
     bhagavan: "Shri Krishna",
+    token: "",
   },
 });
+
+function login() {
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      client_id: "602ac6711be930a2233d8921!",
+      client_secret: "602ac7263e915c5ec230bec6!",
+    }),
+  };
+  fetch("/auth/oauth/v1/token", requestOptions)
+    .then(function (response) {
+      return response.json();
+    })
+    .then((data) => {
+      localStorage.setItem("token", data.token);
+    });
+}
